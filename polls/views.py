7f038dd64b85445b3a9df83ab2484bbd987c33ce
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-05-22 23:20:54 +0300

    changed views index, detail and added views results, vote and html pages

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,13 +14,25 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(models.Question, pk=question_id)
+    if request.method == 'POST':
+        question.delete()
+        return HttpResponseRedirect(reverse('polls:index'))
     return render(request, 'polls/detail.html', {'question': question, })
 
 
 def results(request, question_id):
-    return HttpResponse('Вы смотрите на результаты вопроса {}'.format(question_id))
+    question = get_object_or_404(models.Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question, })
 
 
 def vote(request, question_id):
-    return HttpResponse('Вы голосуете по вопросу {}'.format(question_id))
+    question = get_object_or_404(models.Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, models.Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': 'Вы не сделали выбор!', })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
 
