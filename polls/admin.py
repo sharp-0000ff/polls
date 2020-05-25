5838233521be0e6b41eeae625625bafe658fb856
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-05-25 16:22:25 +0300

    changed admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from  . import models
+from . import models
 
 # Register your models here.
 
-admin.site.register(models.Question)
-admin.site.register(models.Choice)
+
+class ChoiceInLine(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'publish_date', 'was_published_recently')
+    list_filter = ['publish_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publish_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInLine]
